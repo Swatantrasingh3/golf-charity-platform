6aea3a2a8500932d0ff6487fb67b1925a8ddc616
Swatantrasingh3 <swatantrasingh068@gmail.com>
2026-03-28 02:00:10 +0530

    Delete draw.js

--- draw.js
+++
@@ -1,9 +0,0 @@
-export function generateDraw() {
-  return Array.from({ length: 5 }, () =>
-    Math.floor(Math.random() * 45) + 1
-  );
-}
-
-export function checkMatch(scores, draw) {
-  return scores.filter((s) => draw.includes(s)).length;
-}
