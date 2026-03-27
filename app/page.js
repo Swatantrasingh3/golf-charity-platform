405a1133f9798092604691761fe9fb88de8f6d2d
Swatantrasingh3 <swatantrasingh068@gmail.com>
2026-03-28 02:11:46 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Golf Charity Platform</h1>
-      <p>Play • Win • Support Charity</p>
-
-      <Link href="/signup">Signup</Link> |{" "}
-      <Link href="/login">Login</Link>
+      <p>Project is working ✅</p>
     </div>
   );
 }
